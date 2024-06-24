686f886e01a81d4986abd568aa2c79d967d2822c
Mary na <tehcentr2014@gmail.com>
2024-06-24 07:37:53 +0000

    to deploy

---
+++ mail.php
@@ -0,0 +1,26 @@
+<?php  
+  
+foreach ($_REQUEST as $key=>$val) {  
+    ${$key}=$val;  
+}  
+  
+  
+  
+//echo $name;  
+//echo "<br>";  
+///echo $email;  
+//echo "<br>";  
+///echo $subject;  
+  
+$message = "$mess \n  $name \n $email";  
+  
+ echo "<b>Mail Send!</b>"; 
+  
+  
+  
+mail('tehcentr1920@gmail.com', $subject, $message);  
+mail('ttarras@gmail.com', $subject, $message);  
+  
+  
+  
+?>
